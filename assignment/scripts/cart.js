edef7607ad98defc572328d203d3050068fc2b2b
Chris Maki <c.william.developer@gmail.com>
2022-09-08 16:57:44 -0500

    Remedied mistake in clearing the array. Provided 3 examples. Now onto stretch goals

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -4,7 +4,8 @@ console.log('***** Cart Functions *****');
 let basket = [];
 
 function addItem(item) {
-    
+    console.log("Current basket:",basket);
+
 
 if(item) {
     console.log("Fruit was added:",true);
@@ -15,10 +16,9 @@ if(item) {
     console.log("---undefined value not added to array---")
     
 }
-console.log("Current basket:",basket);
 
 };
- 
+
 addItem("Apple");
 addItem( );
 addItem("Mango");
@@ -26,6 +26,7 @@ addItem("Papaya");
 addItem();
 addItem("Cherry");
 
+console.log("Current basket:",basket);
 
 //I had it written previously where the array would return the
 // undefined value, but I figured it made more sense to omit it
@@ -46,7 +47,37 @@ listItems(basket);
 
 function empty(arr) {
     arr = [];
-    console.log(arr);
-}
+    return arr;
+ }
+ 
+  console.log(empty(basket)); 
+  console.log("Still globally returns",basket);
 
-empty(basket);
+ // End emptying array example 1.
+
+  function empty2(arr) {
+    arr = [];
+    console.log(arr);
+  }
+ 
+empty2(basket);
+console.log("Still globally returns",basket);
+ 
+ // End emptying array example 2.
+  function empty3(arr) {
+      arr.length = 0;
+      console.log("NOW the global basket array is",basket);
+  }
+ empty3(basket);
+ // End emptying array example 3.
+ addItem("Apple"); // Called addItem again to test it. 
+
+ console.log(basket);
+
+// So, I provided three different methods for emptying the array. The first two empty
+// the array, however they create NEW arrays. If I console.log(basket) after the first
+// two functions, basket will return with no changes, as shown when I console.log(basket).
+// For the third function, setting basket.length to 0 will specifically affect
+// the global basket variable without creating a new array. I'm not fully sure
+// why this is, and sort of stumbled upon it by accident, but there ya go!
+
